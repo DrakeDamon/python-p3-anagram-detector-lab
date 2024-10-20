da915c88441a064678f8262747923a2cbf40bb38
Drake Damon <dddamon06@gmail.com>
2024-10-20 09:54:57 -0400

    completed anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word 
+
+  def match(self, word_list):
+    matches = []  # Empty list to store matches
+    sorted_self_word = sorted(self.word)  # Sort the initialized word
+
+    for word in word_list:  # Loop through each word in word_list
+        if sorted(word) == sorted_self_word:  # Check if the sorted word matches the sorted initialized word
+            matches.append(word)  # If it matches, append to matches
+
+    return matches  # Return the matches list after the loop completes
